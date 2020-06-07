a0171d1b35b48e098779c51f4a164c8073d957ed
AbelAlejandro <abel.alejandro@ymail.com>
2020-06-07 21:10:09 +0100

    Added getters and setters to POJOs

--- src/main/java/abel/springframework/sfgpetclinic/model/Owner.java
+++ src/main/java/abel/springframework/sfgpetclinic/model/Owner.java
@@ -1,4 +1,4 @@
 package abel.springframework.sfgpetclinic.model;
 
-public class Owner {
+public class Owner extends Person {
 }
--- src/main/java/abel/springframework/sfgpetclinic/model/Vet.java
+++ src/main/java/abel/springframework/sfgpetclinic/model/Vet.java
@@ -1,4 +1,4 @@
 package abel.springframework.sfgpetclinic.model;
 
-public class Vet {
+public class Vet extends Person {
 }
