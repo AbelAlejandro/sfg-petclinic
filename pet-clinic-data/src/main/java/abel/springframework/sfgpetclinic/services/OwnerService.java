09845634203b63b92e4339c496e6b9b0a91e77ab
AbelAlejandro <abel.alejandro@ymail.com>
2020-06-08 17:16:04 +0100

    Refactored interfaces. Closes #11

--- pet-clinic-data/src/main/java/abel/springframework/sfgpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/abel/springframework/sfgpetclinic/services/OwnerService.java
@@ -2,11 +2,6 @@ package abel.springframework.sfgpetclinic.services;
 
 import abel.springframework.sfgpetclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 }
