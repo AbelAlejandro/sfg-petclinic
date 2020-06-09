f5ae1f583f1c4b38a7d42aa9f2c1425cc0130842
AbelAlejandro <abel.alejandro@ymail.com>
2020-06-09 16:45:28 +0100

    Added vet index. Closes #7

---
+++ pet-clinic-web/src/main/java/abel/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,12 @@
+package abel.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class VetController {
+    @GetMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets() {
+        return "vets/index";
+    }
+}
