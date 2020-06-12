08c989922476466628ec268d4a5bfcc9434cc65c
AbelAlejandro <abel.alejandro@ymail.com>
2020-06-12 13:49:41 +0100

    Added PetType Map Service. Closes #29

---
+++ pet-clinic-data/src/main/java/abel/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package abel.springframework.sfgpetclinic.services;
+
+import abel.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
