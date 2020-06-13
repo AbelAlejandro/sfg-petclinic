405fedac93c3d42262c45037e65e814e5afec262
AbelAlejandro <abel.alejandro@ymail.com>
2020-06-13 13:42:20 +0100

    Added Owner Spring Data JPA Service. Closes #40

--- pet-clinic-data/src/main/java/abel/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/abel/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,5 @@ import abel.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    Owner findByLastName(String lastName);
 }
