f877a1e01079c011fab9718608b30f84809a96ef
AbelAlejandro <abel.alejandro@ymail.com>
2020-06-13 13:01:37 +0100

    Added JPA to Owners, Pet and PetType. Closes #36

--- pet-clinic-data/src/main/java/abel/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/abel/springframework/sfgpetclinic/model/Owner.java
@@ -1,12 +1,19 @@
 package abel.springframework.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+    @Column(name = "address")
     private String address;
+    @Column(name = "city")
     private String city;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
+    @Column(name = "telephone")
     private String telephone;
 
     public String getAddress() {
