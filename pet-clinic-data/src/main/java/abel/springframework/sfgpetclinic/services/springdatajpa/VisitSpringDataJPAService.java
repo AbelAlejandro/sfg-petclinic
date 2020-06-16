9e99f7b2e36a0e869ee169d3ca6a4f16e95bca89
AbelAlejandro <abel.alejandro@ymail.com>
2020-06-16 19:29:01 +0100

    Added Visit Spring Data JPA service. Closes #47

---
+++ pet-clinic-data/src/main/java/abel/springframework/sfgpetclinic/services/springdatajpa/VisitSpringDataJPAService.java
@@ -0,0 +1,47 @@
+package abel.springframework.sfgpetclinic.services.springdatajpa;
+
+import abel.springframework.sfgpetclinic.model.Visit;
+import abel.springframework.sfgpetclinic.repositories.VisitRepository;
+import abel.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSpringDataJPAService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitSpringDataJPAService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
