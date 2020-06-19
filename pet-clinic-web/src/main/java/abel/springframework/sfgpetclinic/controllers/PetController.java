8252231f52cc26495ea01bbb493ac9f174a0e920
AbelAlejandro <abel.alejandro@ymail.com>
2020-06-19 22:11:35 +0100

    Fixed test

--- pet-clinic-web/src/main/java/abel/springframework/sfgpetclinic/controllers/PetController.java
+++ pet-clinic-web/src/main/java/abel/springframework/sfgpetclinic/controllers/PetController.java
@@ -49,7 +49,6 @@ public class PetController {
     @GetMapping("/pets/new")
     public String initCreationForm(Owner owner, Model model) {
         Pet pet = new Pet();
-        owner.getPets().add(pet);
         pet.setOwner(owner);
         model.addAttribute("pet", pet);
         return VIEWS_PETS_CREATE_OR_UPDATE_FORM;
